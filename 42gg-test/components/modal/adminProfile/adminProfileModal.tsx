4c3aa8bdb3a03e2fa4c10b2323aa1bfae2b03701
김성욱 <sungwook@MacAir.local>
2023-01-30 16:21:45 +0900

    edit admin_profile

--- 42gg-test/components/modal/adminProfile/adminProfileModal.tsx
+++ 42gg-test/components/modal/adminProfile/adminProfileModal.tsx
@@ -9,16 +9,30 @@ import Image from 'next/image';
 
 export default function AdminProfileModal(user: any) {
   const [userInfo, setUserInfo] = useState<any>();
-  //   currentExp
-  //   expRate
+  //   userId
   //   intraId
-  //   level
-  //   maxExp
-  //   racketType
-  //   rivalRecord,  statusMessage, userImageUri
+  //   userImageUrl
+  //   racket_type
+  //   status_message
+  //   wins
+  //   losses
+  //   ppp
+  //   e_mail
+  //   role_type
 
   const [userProfileImg, setUserProfileImg] = useState<File>();
   const [userPreviewImg, setPreviewImg] = useState<string>();
+  const racketTypes = [
+    { id: 'penholder', label: 'PENHOLDER' },
+    { id: 'shakehand', label: 'SHAKEHAND' },
+    { id: 'dual', label: 'DUAL' },
+  ];
+  const onChange = ({target: {name, value},}: any | any) => {
+    setUserInfo((current: any) => ({
+      ...current,
+      [name]: value,
+    }));
+  };
   useEffect(() => {
     if (userProfileImg) {
       const reader = new FileReader();
@@ -44,9 +58,10 @@ export default function AdminProfileModal(user: any) {
   }, []);
 
   const getBasicProfileHandler = async () => {
-    const res = await instance.get(`/pingpong/users/sungwook/detail`);
+    const res = await fetch(`http://localhost:3000/api/admin/users`);
+    const data = await res.json();
     // res?.data
-    setUserInfo({ ...res.data }); // deep copy X, shallow copy O
+    setUserInfo(data[0]); // deep copy X, shallow copy O
     // setUserInfo(res.data);
   };
 
@@ -63,7 +78,7 @@ export default function AdminProfileModal(user: any) {
                   src={
                     userPreviewImg
                       ? userPreviewImg
-                      : `${userInfo?.userImageUri}`
+                      : `${userInfo?.userImageUrl}`
                   }
                   width='150'
                   height='150'
@@ -73,27 +88,77 @@ export default function AdminProfileModal(user: any) {
                   type='file'
                   style={{ display: 'none' }}
                   onChange={photoUpload}
-                ></input>
+                />
               </label>
               <br />
-              <label>ID</label>
+              <label>ID : {userInfo?.intraId}</label>
               <br />
-              <label>이메일</label>
+              <label>
+                이메일:{' '}
+                <input
+                  name='e_mail'
+                  onChange={onChange}
+                  value={userInfo?.e_mail}
+                />
+              </label>
               <br />
-              <label>상태 메시지</label>
+              <label>
+                상태 메시지:
+                <input
+                  name='status_message'
+                  onChange={onChange}
+                  value={userInfo?.status_message}
+                />
+              </label>
               <br />
-              <label>Racket type : </label>
+              {racketTypes.map((racket, index) => {
+                return (
+					<label key={index} htmlFor={racket.id}>
+						<div>{racket.label}</div>
+						<input
+							type='radio'
+							name='racket_type'
+							id={racket.id}
+							value={racket.id}
+							onChange={onChange}
+							checked={userInfo?.racket_type === racket.id}
+						/>
+                  </label>
+                );
+              })}
               <br />
-              <label>승 :</label>
+              <label>
+                승 :
+                <input
+                  id='wins'
+                  onChange={onChange}
+                  value={userInfo?.wins}
+                />
+              </label>
               <br />
-              <label>패 :</label>
+              <label>
+                패 :
+                <input
+                  id='losses'
+                  onChange={onChange}
+                  value={userInfo?.losses}
+                />
+              </label>
               <br />
-              <label>PPP :</label>
+              <label>
+                PPP :
+                <input
+                  id='ppp'
+                  onChange={onChange}
+                  value={userInfo?.ppp}
+                />
+              </label>
             </div>
           </div>
           <div className={styles.buttons}>
             <div className={styles.negative}></div>
             <div className={styles.positive}>
+              <button onClick={() => console.log(userInfo)}>Edit</button>
               <button onClick={() => setModal({ modalName: null })}>
                 CANCEL
               </button>
